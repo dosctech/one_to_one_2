1303fbc86d2e7ec281797757e434f93b90a61756
VernPherus <vernpherus@gmail.com>
2024-01-21 16:58:33 +0800

    created models and migration

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index(){
+        $students = Student::with('academic', 'country')->get();
+        return response()->json(['students'=>$students]);
+    }
+
+    public function store(Request $request){
+        $student = Student::create($request->all());
+        $student->academic()->create($request->input('academic'));
+        $student->country()->create($request->input('country'));
+        return response()->json($student, 201);
+    }
+
+    public function update(Request $request, $id){
+        $student = Student::find($id);
+        $student = update($request->all());
+        $student->academic()->update($request->input('academic'));
+        $student->country()->update($request->input('country'));
+        return response()->json(['student'=>$student]);
+    }
+
+    public function destroy($id){
+        $student = Student::find($id);
+        $student->academic()->delete();
+        $student->country()->delete();
+        $student->delete();
+        return response()->json(["message"=>"Succssfully deleted student record."]);
+    }
+}
